515962bb6a2781dfe3bf727f1d9ee3668819cb1e
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2025-09-17 12:43:11 -0400

    refactor: remove unused functions _build_cutflow and dump_friend from dump_friendtrees.py

--- analysis/helpers/dump_friendtrees.py
+++ analysis/helpers/dump_friendtrees.py
@@ -4,33 +4,11 @@ import numpy as np
 from src.data_formats.root import Chunk, Friend
 from src.storage.eos import PathLike
 from src.aktools import has_record, get_field
+from src.friendtrees.dump_friend import dump_friend, _build_cutflow
 
 _NAMING = "{path1}/{name}_{uuid}_{start}_{stop}_{path0}"
 
 # TODO: dump trigger weight?
-
-
-def _build_cutflow(*selections):
-    if len(selections) == 1:
-        return selections[0]
-    selection = akext.to_numpy(selections[0], copy=True).astype(bool)
-    for s in selections[1:]:
-        selection[selection] = s
-    return ak.Array(selection)
-
-
-def dump_friend(
-    events: ak.Array,
-    output: PathLike,
-    name: str,
-    data: ak.Array,
-    dump_naming: str = _NAMING,
-):
-    chunk = Chunk.from_coffea_events(events)
-    friend = Friend(name)
-    friend.add(chunk, data)
-    friend.dump(output, dump_naming)
-    return {name: friend}
 
 
 def dump_input_friend(
