3cd12e9276b77bb52f17107405db4d7e21da5ebc
John Alison <johnda102@gmail.com>
2025-10-07 11:57:24 -0400

    Starting to update plots for PAS

--- scripts/makeplotsForPaper.sh
+++ scripts/makeplotsForPaper.sh
@@ -1,31 +1,32 @@
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/mixedVsDataVs3b/RunII/passPreSel/fourTag/SR/SvB_MA_noFvT_ps_hh_ave_logy_fixed_binning.yaml   -o plotsForPaper/mixedVsDataVs3b
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/mixedVsDataVs3b/RunII/passPreSel/fourTag/SB/SvB_MA_noFvT_ps_hh_ave_logy_fixed_binning.yaml   -o plotsForPaper/mixedVsDataVs3b
-
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_ps_hh_logy_fixed_binning.yaml -o plotsForPaper
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_noFvT_ps_hh_logy_fixed_binning.yaml  -o plotsForPaper
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/mixedVsDataVs3b/RunII/passPreSel/fourTag/SR/SvB_MA_noFvT_ps_hh_ave_logy_fixed_binning.yaml   -o plotsForPaper_output/mixedVsDataVs3b
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/mixedVsDataVs3b/RunII/passPreSel/fourTag/SB/SvB_MA_noFvT_ps_hh_ave_logy_fixed_binning.yaml   -o plotsForPaper_output/mixedVsDataVs3b
 
 
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/SvB_MA_postfitplots_prefit_138.yaml   -o plotsForPaper/
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/SvB_MA_postfitplots_fit_b_138.yaml   -o plotsForPaper/
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/SvB_MA_postfitplots_fit_s_138.yaml   -o plotsForPaper/
-
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/SvB_MA_postfitplots_fit_b.yaml   -o plotsForPaper/
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/SvB_MA_postfitplots_prefit.yaml   -o plotsForPaper/
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/SvB_MA_postfitplots_fit_s.yaml   -o plotsForPaper/
-
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/Mixeddata_SvB_MA_postfitplots_fit_b.yaml   -o plotsForPaper/
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/Mixeddata_SvB_MA_postfitplots_prefit.yaml   -o plotsForPaper/
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/Mixeddata_SvB_MA_postfitplots_fit_s.yaml   -o plotsForPaper/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_ps_hh_logy_fixed_binning.yaml   -o plotsForPaper_output
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_noFvT_ps_hh_logy_fixed_binning.yaml  -o plotsForPaper_output
 
 
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/SvB_MA_postfitplots_prefit_138.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/SvB_MA_postfitplots_fit_b_138.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/SvB_MA_postfitplots_fit_s_138.yaml   -o plotsForPaper_output/
 
-python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files plotsForPaper/0_variance_multijet_ensemble_basis0.yaml   -o plotsForPaper/
-python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files plotsForPaper/0_variance_multijet_ensemble_basis1.yaml   -o plotsForPaper/
-python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files plotsForPaper/0_variance_multijet_ensemble_basis2.yaml   -o plotsForPaper/
-python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files plotsForPaper/0_variance_multijet_ensemble_basis3.yaml   -o plotsForPaper/
-python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files plotsForPaper/0_variance_multijet_ensemble_basis4.yaml   -o plotsForPaper/
-python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files plotsForPaper/0_variance_multijet_ensemble_basis5.yaml   -o plotsForPaper/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/SvB_MA_postfitplots_fit_b.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/SvB_MA_postfitplots_prefit.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/SvB_MA_postfitplots_fit_s.yaml   -o plotsForPaper_output/
 
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/1_bias_basis-1.yaml    -o plotsForPaper/ 
-python coffea4bees/plots/plot_from_yaml.py --input_yaml_files plotsForPaper/1_bias_basis0.yaml    -o plotsForPaper/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/Mixeddata_SvB_MA_postfitplots_fit_b.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/Mixeddata_SvB_MA_postfitplots_prefit.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/Mixeddata_SvB_MA_postfitplots_fit_s.yaml   -o plotsForPaper_output/
+
+
+
+python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/0_variance_multijet_ensemble_basis0.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/0_variance_multijet_ensemble_basis1.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/0_variance_multijet_ensemble_basis2.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/0_variance_multijet_ensemble_basis3.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/0_variance_multijet_ensemble_basis4.yaml   -o plotsForPaper_output/
+python coffea4bees/plots/variance_plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/0_variance_multijet_ensemble_basis5.yaml   -o plotsForPaper_output/
+
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/1_bias_basis-1.yaml    -o plotsForPaper_output/ 
+python coffea4bees/plots/plot_from_yaml.py --input_yaml_files coffea4bees/archive/plotsForPaper/1_bias_basis0.yaml    -o plotsForPaper_output/
 
