cc251d07dd601d5daab5d457de76adc2fbb6c8aa
John Alison <johnalison@cmu.edu>
2026-01-08 11:20:31 -0600

    Fix CI v2
    - Introduced dynamic input directory handling for hemisphere metadata
    - Added logic to modify metadata paths and adapt to runner-specific settings
    - Updated script to use dynamically generated metadata file for processing

--- scripts/mixeddata-study-hemispheres.sh
+++ scripts/mixeddata-study-hemispheres.sh
@@ -9,11 +9,20 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+INPUT_DIR="$OUTPUT_BASE_DIR"
 JOB="test_study_hemispheres"
 OUTPUT_DIR=$OUTPUT_BASE_DIR/$JOB
-#!/bin/bash
+
+display_section_header "Changing hemisphere library metadata"
+HEMI_LIB="$OUTPUT_DIR/hemisphere_library_for_test.yml"
+sed -e "s|output|$INPUT_DIR|" \
+    coffea4bees/skimmer/metadata/hemisphere_library_test.yml > $HEMI_LIB
+[[ $(hostname) = *runner* ]] && sed -i "s|T3_US_FNALLPC|T3_CH_PSI|" $HEMI_LIB
+cat $HEMI_LIB; echo
+
+
 
 echo "############### Running hemisphere mixing test"
 #python coffea4bees/hemisphere_mixing/tests/test_mixing.py 
 
-python coffea4bees/hemisphere_mixing/study_hemispheres.py --hemifiles coffea4bees/skimmer/metadata/hemisphere_library_test.yml --year UL18  --threshold 10 --output_path $OUTPUT_DIR
+python coffea4bees/hemisphere_mixing/study_hemispheres.py --hemifiles $HEMI_LIB  --year UL18  --threshold 10 --output_path $OUTPUT_DIR
