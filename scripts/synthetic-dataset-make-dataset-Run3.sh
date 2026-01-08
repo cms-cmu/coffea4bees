1b00bb407edf9697e548a93da6ac00917102c033
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-01-08 11:42:15 -0500

    Update synthetic dataset test configuration and comment out unused DATASETS variable

--- scripts/synthetic-dataset-make-dataset-Run3.sh
+++ scripts/synthetic-dataset-make-dataset-Run3.sh
@@ -11,6 +11,7 @@ if [ $? -ne 0 ]; then
 fi
 
 display_section_header "Input Datasets"
+# DATASETS=${DATASET_RUN3:-"coffea4bees/metadata/datasets_synthetic_test_Run3.yml"}
 DATASETS="coffea4bees/metadata/datasets_synthetic_test_Run3.yml"
 echo "Using datasets file: $DATASETS"
 
