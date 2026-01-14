c5d0bcae4c69ab1eb04b1448c293cf8211ab2063
John Alison <johnda102@gmail.com>
2026-01-14 09:56:07 -0500

    Add support for TwoTag cut flow in printCutFlow script
    - Updated `printCF` function to handle TwoTag cut flow data
    - Introduced `doTwoTag` flag to conditionally process TwoTag data
    - Refactored era handling with `era_aliases` for better flexibility
    - Enhanced argument parsing to support new era aliases
    - Adjusted weighted corrections and data aggregation for TwoTag cut flow
    
    This update enables the script to process and display TwoTag cut flow data alongside existing FourTag and ThreeTag data.

--- analysis/printCutFlow.py
+++ analysis/printCutFlow.py
@@ -15,19 +15,43 @@ def _round(val):
     return round(float(val),1)
 
 def printLine(words):
-    print(f'\t{words[0]:<30}\t{words[1]:<10}   {words[2]:<10} \t\t {words[3]:<10}\t{words[4]:<10}')
+    for iW in range(len(words)):
+        if iW == 0:
+            print(f'\t{words[iW]:<30}', end='')
+        else:
+            if iW % 2 == 0:
+                print(f'\t{words[iW]:<10}', end='')
+            else:
+                print(f'\t\t{words[iW]:<10}', end='')
+    print('')
 
-def printCF(procKey, cf4, cf4_unit, cf3, cf3_unit):
+def printCF(procKey, cf4, cf4_unit, cf3, cf3_unit, cf2=None, cf2_unit=None):
 
     bar = "-"*10
     print('\n')
     print(procKey,':\n')
-    printLine(["Cuts","FourTag","","ThreeTag",""])
-    printLine(["",bar,bar,bar,bar])
-    printLine(["","weighted","(unit weight)","weighted","(unit weight)"])
+
+    cuts_line = ["Cuts","FourTag","","ThreeTag",""]
+    if cf2: cuts_line += ["TwoTag",""]
+    printLine(cuts_line)
+
+    bar_line = ["",bar,bar,bar,bar]
+    if cf2: bar_line += [bar,bar]
+    printLine(bar_line)
+
+    weight_line = ["","weighted","(unit weight)","weighted","(unit weight)"]
+    if cf2: weight_line += ["weighted","(unit weight)"]
+    printLine(weight_line)
     print('\n')
+
     for cut in cf4.keys():
-        printLine([cut,_round(cf4[cut]),_round(cf4_unit[cut]),_round(cf3[cut]),_round(cf3_unit[cut])])
+        counts_line =  [cut,
+                        _round(cf4[cut]), _round(cf4_unit[cut]),
+                        _round(cf3[cut]), _round(cf3_unit[cut])]
+        if cf2:
+               counts_line += [ _round(cf2[cut]), _round(cf2_unit[cut])]
+
+        printLine(counts_line)
 
     print("\n")
 
@@ -38,23 +62,34 @@ def add(thisKey):
 
 if __name__ == '__main__':
 
-    run2_eras_data = ["UL16_postVFPF" , "UL16_postVFPG" , "UL16_postVFPH" , "UL16_preVFPB" , "UL16_preVFPC" , "UL16_preVFPD" , "UL16_preVFPE" , "UL17C" , "UL17D" , "UL17E" , "UL17F" , "UL18A" , "UL18B" , "UL18C" , "UL18D"]
-    run2_eras_mc   = ["UL16_postVFP" , "UL16_preVFP" , "UL17" ,  "UL18"]
+    era_aliases = {}
+    era_aliases["Run2"] = ["UL16_postVFPF" , "UL16_postVFPG" , "UL16_postVFPH" , "UL16_preVFPB" , "UL16_preVFPC" , "UL16_preVFPD" , "UL16_preVFPE" , "UL17C" , "UL17D" , "UL17E" , "UL17F" , "UL18A" , "UL18B" , "UL18C" , "UL18D"]
+    era_aliases["Run2MC"] = ["UL16_postVFP" , "UL16_preVFP" , "UL17" ,  "UL18"]
+    era_aliases["2022_preEE"] = ["2022_preEEB", "2022_preEEC", "2022_preEED"]
+    era_aliases["2022_EE"] = ["2022_EEE", "2022_EEF", "2022_EEG"]
+    era_aliases["2023_preBPix"] = ["2023_preBPixA", "2023_preBPixB", "2023_preBPixC", "2023_preBPixD", "2023_preBPixE", "2023_preBPixF"]
+    era_aliases["2023_BPix"] = ["2023_BPixD",'2023_BPixE']
+
 
     parser = argparse.ArgumentParser(description='uproot_plots')
     parser.add_argument('-i','--inputFile', default='hists.pkl', help='Input File. Default: hists.pkl')
     parser.add_argument('-p','--process',   default='data', help='Input process. Default: hists.pkl')
-    parser.add_argument('-e','--era',   nargs='+', dest='eras', default=run2_eras_data, help='Input process. Default: hists.pkl')
+    parser.add_argument('-e','--era',   nargs='+', dest='eras', default=era_aliases["Run2"], help='Input process. Default: hists.pkl')
     #parser.add_argument('-d', '--datasets', nargs='+', dest='datasets', , help="Name of dataset to run. Example if more than one: -d HH4b ZZ4b")
     args = parser.parse_args()
 
     if "Run2" in args.eras:
         if args.process == "data":
-            eras = run2_eras_data
+            eras = era_aliases["Run2"]
         else:
-            eras = run2_eras_mc
+            eras = era_aliases["Run2MC"]
     else:
-        eras = args.eras
+        eras = []
+        for e in args.eras:
+            if e in era_aliases:
+                eras += era_aliases[e]
+            else:
+                eras += args.eras
 
     eraString = "_".join(eras)
     print(eras)
@@ -65,14 +100,24 @@ if __name__ == '__main__':
         print("Have yml")
         in_file = yaml.safe_load(open(args.inputFile, 'r'))
 
+        doTwoTag = True
+        if not "cutFlowTwoTag" in in_file[key]:
+            doTwoTag = False
+
+
         cf4 = {}
         cf4_unit = {}
         cf3 = {}
         cf3_unit = {}
+        if doTwoTag:
+            cf2 = {}
+            cf2_unit = {}
+
 
         cf4_unit[key] = in_file[key]["cutFlowFourTagUnitWeight"]
-
         cf3_unit[key] = in_file[key]["cutFlowThreeTagUnitWeight"]
+        if doTwoTag:
+            cf2_unit[key] = in_file[key]["cutFlowTwoTagUnitWeight"]
 
         genEventSumw = in_file[key]["sumw"]
         lumi         = in_file[key]["lumi"][0]
@@ -88,6 +133,12 @@ if __name__ == '__main__':
         for k, v in cf3[key].items():
             cf3[key][k] *= weighted_correction
 
+        if doTwoTag:
+            cf2[key]      = in_file[key]["cutFlowTwoTag"]
+            for k, v in cf2[key].items():
+                cf2[key][k] *= weighted_correction
+
+
 
     else:
         with open(f'{args.inputFile}', 'rb') as hfile:
@@ -95,10 +146,18 @@ if __name__ == '__main__':
             hists = load(hfile)
             #load_hists(args.inputFile)
 
+        doTwoTag = True
+        if not "cutFlowTwoTag" in hists:
+            doTwoTag = False
+
         cf4      = hists["cutFlowFourTag"]
         cf4_unit = hists["cutFlowFourTagUnitWeight"]
         cf3      = hists["cutFlowThreeTag"]
         cf3_unit = hists["cutFlowThreeTagUnitWeight"]
+        if doTwoTag:
+            cf2      = hists["cutFlowTwoTag"]
+            cf2_unit = hists["cutFlowTwoTagUnitWeight"]
+
 
 
     if key not in cf4:
@@ -109,6 +168,10 @@ if __name__ == '__main__':
         cf3      [key] = {}
         cf3_unit [key] = {}
 
+        if doTwoTag:
+            cf2      [key] = {}
+            cf2_unit [key] = {}
+
         for e in eras:
 
             for cut, v in cf4[args.process+"_"+e].items():
@@ -117,10 +180,21 @@ if __name__ == '__main__':
                 if cut not in cf3[key]:      cf3[key][cut] = 0
                 if cut not in cf3_unit[key]: cf3_unit[key][cut] = 0
 
+                if doTwoTag:
+                    if cut not in cf2[key]:      cf2[key][cut] = 0
+                    if cut not in cf2_unit[key]: cf2_unit[key][cut] = 0
+
+
                 cf4[key][cut]      += cf4[args.process+"_"+e][cut]
                 cf4_unit[key][cut] += cf4_unit[args.process+"_"+e][cut]
                 cf3[key][cut]      += cf3[args.process+"_"+e][cut]
                 cf3_unit[key][cut] += cf3_unit[args.process+"_"+e][cut]
+                if doTwoTag:
+                    cf2[key][cut]      += cf2[args.process+"_"+e][cut]
+                    cf2_unit[key][cut] += cf2_unit[args.process+"_"+e][cut]
 
-    #cutList = hists["cutFlowThreeTagUnitWeight"][key].keys()
-    printCF(key, cf4[key], cf4_unit[key], cf3[key], cf3_unit[key])
+
+    if doTwoTag:
+        printCF(key, cf4[key], cf4_unit[key], cf3[key], cf3_unit[key], cf2[key], cf2_unit[key])
+    else:
+        printCF(key, cf4[key], cf4_unit[key], cf3[key], cf3_unit[key])
