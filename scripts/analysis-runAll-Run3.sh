76e3e86b4c3baa88268dbbd0207c72964c4cbb1c
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-01-16 12:30:46 -0600

    Add synthetic data configuration and update dataset paths in analysis scripts
    - Created a new YAML file for synthetic data configurations under metadata/datasets_HH4b_Run3/synthetic_data.yml.
    - Updated dataset paths in analysis-runAll-Run3.sh, analysis-test-Run3.sh, mixeddata-make-dataset-Run3-all.sh, and mixeddata-make-dataset-Run3.sh to point to the new directory structure.
    - Modified skimmer-fourTag-job.sh to reference the updated dataset path for Run3.

--- scripts/analysis-runAll-Run3.sh
+++ scripts/analysis-runAll-Run3.sh
@@ -15,7 +15,7 @@ OUTPUT_DIR=$OUTPUT_BASE_DIR/$JOB
 create_output_directory "$OUTPUT_DIR"
 
 display_section_header "Input Datasets"
-DATASETS=${DATASET:-"coffea4bees//metadata/datasets_HH4b_Run3_2025_Run3_skims.yml"}
+DATASETS=${DATASET:-"coffea4bees//metadata/datasets_HH4b_Run3/"}
 echo "Using datasets file: $DATASETS"
 #TTToHadronic TTToSemiLeptonic TTTo2L2Nu
 
