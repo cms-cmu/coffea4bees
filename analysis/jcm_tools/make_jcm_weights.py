71b9d3de50457ec691c0fbe7251641bcdf206e8b
John Alison <johnalison@cmu.edu>
2026-02-12 13:28:05 -0600

    First try at addin tt4bSF parameter to jet combinatoric model
    - Introduced a new parameter `tt4bSF` to the `jetCombinatoricModel` class
      for scaling ttbar 4b contributions.
    - Updated background function logic to incorporate `tt4bSF` in calculations.
    - Enhanced `nTagPred` and `getPseudoTagProbs` methods to handle the new
      parameter.
    - Modified `make_jcm_weights.py` to support fixing `tt4bSF` via CLI
      arguments and added relevant logging.
    - Adjusted parameter handling and plotting functions to include `tt4bSF`.

--- analysis/jcm_tools/make_jcm_weights.py
+++ analysis/jcm_tools/make_jcm_weights.py
@@ -188,14 +188,25 @@ def setup_model(bin_data: Tuple, args: argparse.Namespace, logger: logging.Logge
         JCM_model.fixParameter_combination({
             "threeTightTagFraction": threeTightTagFraction,
             "pairEnhancement": 0.0,
-            "pairEnhancementDecay": 1.0
+            "pairEnhancementDecay": 1.0,
+            "tt4bSF": 1.0
         })
+
     elif args.fix_d:
         logger.info("Fixing pairEnhancementDecay parameter to 1.0")
         JCM_model.fixParameter_combination({
             "threeTightTagFraction": threeTightTagFraction,
-            "pairEnhancementDecay": 1.0
+            "pairEnhancementDecay": 1.0,
+            "tt4bSF": 1.0
         })
+
+    elif args.fix_tt4bSF:
+        logger.info("Fixing ttbar4b SF to 1.0")
+        JCM_model.fixParameter_combination({
+            "threeTightTagFraction": threeTightTagFraction,
+            "tt4bSF": 1.0
+        })
+
     else:
         logger.info(f"Fixing threeTightTagFraction to {threeTightTagFraction:.6f}")
         JCM_model.fixParameter_combination({
@@ -489,7 +500,8 @@ def create_plots(
         plot_param_name = {
             "pseudoTagProb": "f",
             "pairEnhancement": "e",
-            "pairEnhancementDecay": "d"
+            "pairEnhancementDecay": "d",
+            "tt4bSF": "tt4bSF"
         }
         for parameter in JCM_model.parameters:
             if parameter["name"] == "threeTightTagFraction":
@@ -512,6 +524,7 @@ def create_plots(
 
     # Plot tagged jets
     try:
+        logger.info("Creating tagged jet multiplicity plot")
         cfg.hists[0]['hists'][tagJets].fill(**dummy_data)
 
         # Get N-tag jet predictions
@@ -597,6 +610,8 @@ def main():
                         help='Fix the pairEnhancement parameter to 0')
     parser.add_argument('-fix_d', action="store_true",
                         help='Fix the pairEnhancementDecay parameter to 1')
+    parser.add_argument('-fix_tt4bSF', action="store_true",
+                        help='Fix the ttbar SF  to 1')
     parser.add_argument('-i', '--inputFile', nargs="+", dest="inputFile",
                         default='hists.pkl', help='Input file(s). Default: hists.pkl')
     parser.add_argument('-o', '--outputDir', dest='outputDir', default="",
