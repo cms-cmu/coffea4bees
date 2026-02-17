791768c9cdf9298f633a39f792cd49d557c7fd2b
John Alison <johnalison@cmu.edu>
2026-02-16 19:20:40 -0600

    Fix CI remove hardcoded synthetic data flag in splitter
    - Eliminated the `isSyntheticData` flag hack from `split_mixed_data.py`
    - Ensures cleaner and more maintainable code by avoiding hardcoded values

--- skimmer/processor/split_mixed_data.py
+++ skimmer/processor/split_mixed_data.py
@@ -66,8 +66,6 @@ class MixedDataSplitter(PicoAOD):
                                           )
             events["Jet"] = jets
 
-        # Hack
-        config["isSyntheticData"] = True
         events = apply_4b_selection(
             events,
             self.corrections_metadata[year],
