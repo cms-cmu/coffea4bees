8ef581e9e9ce9ad9e7f24ac274ab14abadb5b860
John Alison <johnalison@cmu.edu>
2026-02-16 19:01:11 -0600

    FixCI update Run3 configuration for jet calibration and HLT decision
    - Refactored Run3-specific settings to streamline configuration logic
    - Disabled jet calibration and veto maps for Run3 by default
    - Enabled HLT decision cuts for Run3 scenarios

--- analysis/helpers/processor_config.py
+++ analysis/helpers/processor_config.py
@@ -40,11 +40,18 @@ def processor_config(processName, dataset, event):
     config["use_prestored_btag_SF"]  = False
     config["do_jet_veto_maps"]       = False   ## false for run2 until check effect
 
+
     if config["isMC"]:
         config["cut_on_lumimask"]     = False
         config["cut_on_HLT_decision"] = False
         config["do_jet_calibration"]  = True
         config["do_MC_weights"]       = True
+
+    if config["isRun3"]:
+        config['do_jet_veto_maps'] = False
+        config['do_jet_calibration'] = False # Need a better name here (Jet calib is applied in Run3 by default !)
+        config["cut_on_HLT_decision"]  = True
+
 
     if config["isSyntheticData"]:
         config["do_lepton_jet_cleaning"]  = False
@@ -90,8 +97,4 @@ def processor_config(processName, dataset, event):
         config["do_jet_calibration"]  = False
         config["do_jet_veto_maps"]       = False
 
-    if config["isRun3"] and config["isMC"]:
-        config['do_jet_veto_maps'] = False
-        config['do_jet_calibration'] = False # Need a better name here (Jet calib is applied in Run3 by default !)
-        config["cut_on_HLT_decision"]  = True
     return config
