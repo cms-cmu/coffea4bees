08b63d5fff435e2f728af4a17dad5c06d2aadf9a
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-03-18 14:12:08 -0400

    refactor: remove unused configuration files and uncomment training commands in run script

--- classifier/config/workflows/HH4b_2024_v2_lowpt/FvT/run.sh
+++ classifier/config/workflows/HH4b_2024_v2_lowpt/FvT/run.sh
@@ -13,20 +13,20 @@ export WFS="coffea4bees/classifier/config/workflows/HH4b_2024_v2_lowpt/FvT"
 # Generate a random port between 10000 and 60000 so jobs don't crash
 export PORT=$(shuf -i 10000-60000 -n 1)
 
-# train with train.yml and common.yml configs
-# ./src/pyml.py \
-#     template "model: ${MODEL}" $WFS/train.yml \
-#     -from $WFS/../common.yml \
-#     -setting Monitor "address: '127.0.0.1:${PORT}'" \
-#     -flag debug # use debug flag
+train with train.yml and common.yml configs
+./src/pyml.py \
+    template "model: ${MODEL}" $WFS/train.yml \
+    -from $WFS/../common.yml \
+    -setting Monitor "address: '127.0.0.1:${PORT}'" \
+    -flag debug # use debug flag
 
-# # plot the AUC and ROC
-# ./src/pyml.py analyze \
-#     --results ${MODEL}/result.json \
-#     -analysis HCR.LossROC \
-#     -setting IO "output: ${PLOT}" \
-#     -setting IO "report: FvT" \
-#     -setting Monitor "address: '127.0.0.1:${PORT}'" 
+# plot the AUC and ROC
+./src/pyml.py analyze \
+    --results ${MODEL}/result.json \
+    -analysis HCR.LossROC \
+    -setting IO "output: ${PLOT}" \
+    -setting IO "report: FvT" \
+    -setting Monitor "address: '127.0.0.1:${PORT}'" 
 
 # evaluate with evaluate.yml and common.yml configs
 ./src/pyml.py \
