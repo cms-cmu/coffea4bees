2d412d709dc6a5a37aae74de506f429e0c9c5078
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-07-16 13:14:44 -0400

    Include manually modified script paths for archived fourTag datasets

--- scripts/synthetic-dataset-closure.sh
+++ scripts/synthetic-dataset-closure.sh
@@ -13,8 +13,8 @@ fi
 
 display_section_header "Running test processor"
 
-#time python runner.py -o synthetic_data_RunII_seedXXX.coffea -d synthetic_data data -p coffea4bees/analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_run_fastTopReco.yml -m coffea4bees/metadata/datasets_HH4b_fourTag.yml
-#time python runner.py -o test_synthetic_data_seedXXX_hTRW.coffea -d synthetic_data  -p coffea4bees/analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_run_fastTopReco.yml -m coffea4bees/metadata/datasets_HH4b_fourTag.yml
+#time python runner.py -o synthetic_data_RunII_seedXXX.coffea -d synthetic_data data -p coffea4bees/analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_run_fastTopReco.yml -m coffea4bees/metadata/archive/Run2_2024_v1/datasets_HH4b_fourTag.yml
+#time python runner.py -o test_synthetic_data_seedXXX_hTRW.coffea -d synthetic_data  -p coffea4bees/analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_run_fastTopReco.yml -m coffea4bees/metadata/archive/Run2_2024_v1/datasets_HH4b_fourTag.yml
 
 #time python runner.py -o synthetic_data_Run3_v5_new_seedXXX.coffea -d synthetic_data data -p coffea4bees/analysis/processors/processor_HH4b.py -y 2022_preEE 2022_EE 2023_preBPix 2023_BPix  -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_run_fastTopReco.yml -m coffea4bees/metadata/datasets_fourTag_v5.yml
 
@@ -26,7 +26,7 @@ time python runner.py -o synthetic_data_closure_Run2_seed0_data_v2.coffea   -d d
 #time python runner.py -o histAll_bkg.coffea            -d TTToHadronic TTToSemiLeptonic TTTo2L2Nu data                         -p coffea4bees/analysis/processors/processor_HH4b.py  -y UL17 UL18 UL16_preVFP UL16_postVFP  -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_run_slowTopReco.yml
 
 
-#time python runner.py -o test_synthetic_data_seedXXX_noPSData.coffea -d synthetic_data  -p coffea4bees/analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_run_fastTopReco.yml -m coffea4bees/metadata/datasets_HH4b_fourTag.yml
+#time python runner.py -o test_synthetic_data_seedXXX_noPSData.coffea -d synthetic_data  -p coffea4bees/analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_run_fastTopReco.yml -m coffea4bees/metadata/archive/Run2_2024_v1/datasets_HH4b_fourTag.yml
 #time python runner.py -o nominal_noTT.coffea -d data -p coffea4bees/analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op ${OUTPUT_DIR} -c coffea4bees/analysis/metadata/HH4b_subtract_tt.yml 
 
 
