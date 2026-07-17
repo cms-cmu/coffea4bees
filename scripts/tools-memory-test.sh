66356c51012f144912dc700b7a59560b32fb9f86
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-07-17 17:56:56 -0400

    Adjust memory test threshold to 1730 MB to accommodate dynamic loading optimization

--- scripts/tools-memory-test.sh
+++ scripts/tools-memory-test.sh
@@ -20,7 +20,7 @@ DATASETS="coffea4bees/metadata/datasets/"
 echo "Using datasets file: $DATASETS"
 
 run_command python src/scripts/memory/memory_test.py \
-    --threshold 2200 \
+    --threshold 1730 \
     -o $OUTPUT_DIR/mprofile_ci_test \
     --script runner.py \
         -o test.coffea -t \
