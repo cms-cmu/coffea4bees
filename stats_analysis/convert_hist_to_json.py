10837e1ebcc4a2d248349286a64440912578b797
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-08-13 17:31:03 -0400

    refactor: reorganize hist_to_json function and improve script structure

--- stats_analysis/convert_hist_to_json.py
+++ stats_analysis/convert_hist_to_json.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 import sys
 import os
+import hist
+import numpy as np
 
-# Find the path to src/tools/convert_hist_to_json.py relative to this script
-script_dir = os.path.dirname(os.path.realpath(__file__))
-barista_dir = os.path.realpath(os.path.join(script_dir, "../.."))
-target_script = os.path.join(barista_dir, "src/tools/convert_hist_to_json.py")
+def hist_to_json( coffea_hist ):
+    """docstring for hist_to_root"""
 
-if os.path.exists(target_script):
-    os.execv(sys.executable, [sys.executable, target_script] + sys.argv[1:])
-else:
-    print(f"Error: Target script {target_script} not found.", file=sys.stderr)
-    sys.exit(1)
+    yhist = {
+        'edges' : coffea_hist.axes[0].edges.tolist(),
+        'centers' : coffea_hist.axes[0].centers.tolist(),
+        'values' : coffea_hist.values().tolist(),
+        'variances' : coffea_hist.variances().tolist(),
+        'underflow_value' : coffea_hist[hist.loc(-np.inf)].value,
+        'underflow_variance' : coffea_hist[hist.loc(-np.inf)].variance,
+        'overflow_value' : coffea_hist[hist.loc(+np.inf)].value,
+        'overflow_variance' : coffea_hist[hist.loc(+np.inf)].variance,
+    }
+    return yhist
+
+if __name__ == '__main__':
+    # Find the path to src/tools/convert_hist_to_json.py relative to this script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    barista_dir = os.path.realpath(os.path.join(script_dir, "../.."))
+    target_script = os.path.join(barista_dir, "src/tools/convert_hist_to_json.py")
+
+    if os.path.exists(target_script):
+        os.execv(sys.executable, [sys.executable, target_script] + sys.argv[1:])
+    else:
+        print(f"Error: Target script {target_script} not found.", file=sys.stderr)
+        sys.exit(1)
