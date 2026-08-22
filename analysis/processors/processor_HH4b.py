320ac256a089f4b5677646915720f6d9398366b9
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-08-22 13:34:28 -0400

    fix(processor): allow isTTForMixed to load FvT in load_FvT

--- analysis/processors/processor_HH4b.py
+++ analysis/processors/processor_HH4b.py
@@ -789,7 +789,7 @@ class HH4bBaseProcessor(processor.ProcessorABC):
         Args:
             event: Event array
         """
-        if self.config["isMC"]:
+        if self.config["isMC"] and not (self.config["isDataForMixed"] or self.config["isTTForMixed"]):
             return
 
         FvT_loaded = False
